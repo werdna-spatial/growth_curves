588c6ad551af4b8be2e200d8772a486f2279b141
Eric <69522926+werdna-spatial@users.noreply.github.com>
2020-08-11 14:04:39 -0400

    Update MemChk_multi_model_comp_F.py

--- src/MemChk_multi_model_comp_F.py
+++ src/MemChk_multi_model_comp_F.py
@@ -1,7 +1,8 @@
 import tracemalloc
-from multi_model_comp_F import *
+from multi_model_comp_F import Model_Run
+
 tracemalloc.start()
-multi_model_comp_F.main()
+Model_Run()
 current, peak = tracemalloc.get_traced_memory()
 print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
 tracemalloc.stop()
