3104ae3a52baefac9874913f2d72ca6f8d4f235e
Eric <69522926+werdna-spatial@users.noreply.github.com>
2020-08-11 16:14:25 -0400

    Update MemChk_multi_model_comp_F.py

--- src/MemChk_multi_model_comp_F.py
+++ src/MemChk_multi_model_comp_F.py
@@ -1,8 +1,17 @@
 import tracemalloc
 import multi_model_comp_F 
+import argparse
 
-tracemalloc.start()
-Model_Run()
-current, peak = tracemalloc.get_traced_memory()
-print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
-tracemalloc.stop()
+parser = argparse.ArgumentParser()
+parser.add_argument('--MemCheck', dest='MemChk', action='store_true')
+parser.add_argument("NumIter")
+parser.set_defaults(MemChk=False, NumIter=1000)
+args = parser.parse_args()
+
+if MemChk:
+  tracemalloc.start()
+Model_Run(NumIter)
+if MemChk:
+  current, peak = tracemalloc.get_traced_memory()
+  print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+  tracemalloc.stop()
