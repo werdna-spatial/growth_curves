14c8824e8d05b2d1b28dc08b6c1be06844d1a998
Eric <69522926+werdna-spatial@users.noreply.github.com>
2020-08-16 23:19:03 -0400

    Update MemChk_multi_model_comp_F.py

--- src/MemChk_multi_model_comp_F.py
+++ src/MemChk_multi_model_comp_F.py
@@ -3,16 +3,17 @@ from multi_model_comp_F import Model_Run
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("RUN_ID")
 parser.add_argument("VALnits")
 parser.add_argument("VALpits")
 parser.add_argument("VALburnin")
-parser.set_defaults(VALnits=1000,VALpits=100,VALburnin=500)
+parser.set_defaults(RUN_ID=123456789,VALnits=1000,VALpits=100,VALburnin=500)
 args = parser.parse_args()
 
 ##tracemalloc.start()
-print("VALnits,VALpits,VALburnin")
-print(args.VALnits,args.VALpits,args.VALburnin, sep=',')
-Model_Run(int(args.VALnits),int(args.VALpits),int(args.VALburnin))
+print("RUN_ID,VALnits,VALpits,VALburnin")
+print(args.RUN_ID,args.VALnits,args.VALpits,args.VALburnin, sep=',')
+Model_Run(int(args.RUN_ID),int(args.VALnits),int(args.VALpits),int(args.VALburnin))
 ##current, peak = tracemalloc.get_traced_memory()
 ##print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
 ##tracemalloc.stop()
